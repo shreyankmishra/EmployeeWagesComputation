fd1f69259e3f77bb797632c94bc338196c4f9b32
[shreyankmishra] <[mishra.shreyank@gmail.com]>
2020-09-30 01:27:39 +0530

    Add: Branch UC1 for Employee Presence

---
+++ EmpWage/EmpWage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EmpWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int checkVariable = random.Next(2);
+
+            if (checkVariable == IS_FULL_TIME)
+                Console.WriteLine("Employee is Present.");
+            else
+                Console.WriteLine("Employee is Absent.");
+        }
+    }
+}
