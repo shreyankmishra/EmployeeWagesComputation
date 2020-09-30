c14e16832f696732ff1a8a664228814202b9ca6f
shreyankmishra <mishra.shreyank@gmail.com>
2020-09-30 22:54:44 +0530

    Refactor: Employee Wage by company

--- EmpWage/EmpWage/Program.cs
+++ EmpWage/EmpWage/Program.cs
@@ -4,39 +4,14 @@ namespace EmpWage
 {
     class Program
     {
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;
-        public static void compEmployeeWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHrsPerMonth)
-        {
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            while (totalEmpHrs <= maxHrsPerMonth && totalWorkingDays < numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-            }
-            int totalEmpWage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("Total Emp Wage for Company " + company + " is " + totalEmpWage);
-        }
         static void Main(string[] args)
         {
-            compEmployeeWage("DMart", 10, 2, 20);
-            compEmployeeWage("Airtel", 30, 3, 50);
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            reliance.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            Console.WriteLine(reliance.toString());
         }
     }
     }
