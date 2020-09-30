94a9253584507b0b7d1695e7e8feae50c18e89f7
[shreyankmishra] <[mishra.shreyank@gmail.com]>
2020-09-30 20:34:21 +0530

    Refactor: Employee Wage using Class Method

--- EmpWage/EmpWage/Program.cs
+++ EmpWage/EmpWage/Program.cs
@@ -4,15 +4,17 @@ namespace EmpWage
 {
     class Program
     {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int NUMBER_OF_WORKING_DAYS = 20;
         public const int MAX_HRS_IN_MONTH = 100;
-        static void Main(string[] args)
+        public static int compEmployeeWage()
         {
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS)
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -32,7 +34,13 @@ namespace EmpWage
                 totalEmpHrs += empHrs;
             }
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            return totalEmpWage;
         }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Total Employee Wage is : " + compEmployeeWage());
+        }
+
     }
-}
+    }
+
